1f68d03ba0daf267f7fb76450d43ada0d608a86b
JCoupel <155698213+JCoupel@users.noreply.github.com>
2024-11-18 23:38:22 -0600

    Added Profile and Menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import AccContainer from "../components/AccContainer/AccContainer";
 import Collection from "../components/Collections/Collection";
 import Card from "../components/Card/Card";
 import Header from "../components/Header/Header";
+import Menu from "../components/Menu/Menu";
 
 export default function Home() {
     return (
       <div className="Home">
           <Header />
+          <Menu />
           <Card />
           <Collection />
           <AccContainer />
