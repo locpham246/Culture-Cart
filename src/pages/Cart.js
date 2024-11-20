68eec4af3e20e15bb8b5d96fab5e692770f9d261
JCoupel <155698213+JCoupel@users.noreply.github.com>
2024-11-19 21:40:05 -0600

    Scrollable Items Added

---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import Lines from "../components/ScreenLines/Lines";
+import SmallHeader from "../components/SmallHeader/SmallHeader";
+
+export default function Cart() {
+    return (
+      <div className="Cart">
+          <SmallHeader/>
+          <Lines/>
+      </div>
+    );
+  }
