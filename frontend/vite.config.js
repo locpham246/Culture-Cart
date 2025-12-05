5caa72ee022ad6af4d4b172781ed20504db7f389
Loc <52533174+locpham246@users.noreply.github.com>
2025-12-05 08:49:31 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from 'fs'; 
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'), 
-      cert: fs.readFileSync('./localhost.pem'),  
-    },
-  },
-});
+export default defineConfig(({ command }) => { 
+    const config = {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    };
+
+    if (command === 'serve') { 
+        if (fs.existsSync('./localhost.pem')) { 
+            config.server = {
+                https: {
+                    key: fs.readFileSync('./localhost-key.pem'), 
+                    cert: fs.readFileSync('./localhost.pem'),  
+                },
+            };
+        }
+    }
+
+    return config;
+}); 
